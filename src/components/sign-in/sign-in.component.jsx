9a9d4de1f9626c41f4bcd9624a1889eb9a0f0b4c
yfarhod <49104417+yfarhod@users.noreply.github.com>
2020-01-17 20:51:51 +0100

    sign-in updated

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -9,7 +12,19 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }       
+        };       
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' })
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value })
     }
 
     render() {
@@ -17,6 +32,26 @@ class SignIn extends React.Component {
             <div className='sign-in'>
               <h2>I already have an account</h2>
               <span>Sign in with your email and password</span>
+
+              <form onSubmit={this.handleSubmit} >
+                  <FormInput 
+                    name='email' 
+                    type="email" 
+                    handleChange={this.handleChange} 
+                    value={this.state.email} 
+                    label='email'
+                    required />
+                  
+                  <FormInput 
+                    name='password' 
+                    type="password" 
+                    value={this.state.email} 
+                    handleChange={this.handleChange}
+                    label='password'
+                    required />
+                  
+                  <CustomButton type="submit">Sign in</CustomButton>
+              </form>
             </div>
         )
     }
