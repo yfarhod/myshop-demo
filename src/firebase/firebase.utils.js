3828c837dfe9b0ae7b3461f3fb4cc76f80e6796b
yfarhod <49104417+yfarhod@users.noreply.github.com>
2020-02-27 16:00:01 +0100

    bringing shop data from firebase to app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,64 +1,80 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyBpc1RMIeQVIQHBdLQN2k_xs6KlDgRG3Qk",
-    authDomain: "myshop-db-3e9a2.firebaseapp.com",
-    databaseURL: "https://myshop-db-3e9a2.firebaseio.com",
-    projectId: "myshop-db-3e9a2",
-    storageBucket: "myshop-db-3e9a2.appspot.com",
-    messagingSenderId: "639058809935",
-    appId: "1:639058809935:web:c82559386f61cdf9db863a",
-    measurementId: "G-8LGY3VEHWP"
-  };
+  apiKey: "AIzaSyBpc1RMIeQVIQHBdLQN2k_xs6KlDgRG3Qk",
+  authDomain: "myshop-db-3e9a2.firebaseapp.com",
+  databaseURL: "https://myshop-db-3e9a2.firebaseio.com",
+  projectId: "myshop-db-3e9a2",
+  storageBucket: "myshop-db-3e9a2.appspot.com",
+  messagingSenderId: "639058809935",
+  appId: "1:639058809935:web:c82559386f61cdf9db863a",
+  measurementId: "G-8LGY3VEHWP"
+};
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-    const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-    if (!snapShot.exists) {
-      const { displayName, email } = userAuth;
-      const createdAt = new Date();
-
-      try {
-        await userRef.set({
-          displayName,
-          email,
-          createdAt,
-          ...additionalData
-        });
-      } catch (error) {
-          console.log('error creating user', error.message);
-      }
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
     }
+  }
 
-    return userRef;
-  };
+  return userRef;
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-    const collectionRef = firestore.collection(collectionKey);
-   
-    const batch = firestore.batch();
-    objectsToAdd.forEach(obj => {
-      const newDocRef = collectionRef.doc();
-      batch.set(newDocRef, obj);
-    });
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
 
-    return await batch.commit();
-  };
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  return await batch.commit();
+};
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
 
-  export default firebase;
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  console.log(transformedCollection);
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
